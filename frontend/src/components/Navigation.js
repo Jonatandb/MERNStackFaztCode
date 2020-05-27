00d921f24d5de7a54f589e5de42ee772e9cc436a
Jonatandb <jonatandb@gmail.com>
2020-05-27 19:31:31 -0300

    Stack MERN #9, React Router & Navegacion
    https://www.youtube.com/watch?v=fiQh6xUDBBQ&list=PLo5lAe9kQrwrGPjhhzejCt3JENYf5uDNf&index=9

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,7 +1,45 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 export default class Navigation extends Component {
   render() {
-    return <div>NAVIGATION</div>;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            NotesApp
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <Link class="nav-link" to="/">
+                  Notes
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/create">
+                  Create Note
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/user">
+                  Create User
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
