34247fef671bcf9205dfacd5cba52e804894fe13
Jonatandb <jonatandb@gmail.com>
2020-05-27 21:57:37 -0300

    Stack MERN #11, React HTTP POST, DELETE
    https://www.youtube.com/watch?v=Hn7mStlvWFE&list=PLo5lAe9kQrwrGPjhhzejCt3JENYf5uDNf&index=11

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -7,12 +7,56 @@ export default class CreateUser extends Component {
     username: "",
   };
 
-  async componentDidMount() {
-    const res = await axios.get("http://localhost:4000/api/users");
-    this.setState({ users: res.data });
+  componentDidMount() {
+    this.getUsers();
   }
 
+  getUsers = async () => {
+    await axios
+      .get("http://localhost:4000/api/users")
+      .then((res) => {
+        this.setState({ users: res.data });
+      })
+      .catch((err) => {
+        this.setError(err);
+      });
+  };
+
   onChangeUserName = (e) => this.setState({ username: e.target.value });
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:4000/api/users", {
+        username: this.state.username,
+      })
+      .then(() => {
+        this.getUsers();
+        this.setState({ username: "" });
+      })
+      .catch((err) => {
+        this.setError(err);
+      });
+  };
+
+  deleteUser = async (id) => {
+    await axios
+      .delete("http://localhost:4000/api/users/" + id)
+      .then(() => {
+        this.getUsers();
+      })
+      .catch((err) => {
+        this.setError(err);
+      });
+  };
+
+  setError = (err) => {
+    this.setState({ error: err });
+    console.log(JSON.stringify(err));
+    setInterval(() => {
+      this.setState({ error: "" });
+    }, 5000);
+  };
 
   render() {
     return (
@@ -20,16 +64,25 @@ export default class CreateUser extends Component {
         <div className="col-md-4">
           <div className="card card-body">
             <h3>Create New User</h3>
-            <form>
+            <form onSubmit={this.onSubmit}>
               <div className="form-group">
                 <input
                   type="text"
                   className="form-control"
                   onChange={this.onChangeUserName}
+                  value={this.state.username}
                 />
               </div>
+              <button type="submit" className="btn btn-primary">
+                Save
+              </button>
             </form>
           </div>
+          {this.state.error && (
+            <div className="alert alert-danger m-2" role="alert">
+              {JSON.stringify(this.state.error.message)}
+            </div>
+          )}
         </div>
         <div className="col-md-8">
           <ul className="list-group">
@@ -37,6 +90,7 @@ export default class CreateUser extends Component {
               <li
                 className="list-group-item list-group-item-action"
                 key={user._id}
+                onDoubleClick={() => this.deleteUser(user._id)}
               >
                 {user.username}
               </li>
